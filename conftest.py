e89742dfe23042e8eac52d8393cb96115503529e
rakxya111 <rakshyabhuju@gmail.com>
2026-09-23 22:15:09 +0545

    Clarify requirement for test folder in conftest.py

--- conftest.py
+++ conftest.py
@@ -42,6 +42,7 @@ def wait(driver):
     return WebDriverWait(driver, config['timeout'])
 
 # This will require a folder name as tests
+# This will require a folder name as tests
 @pytest.hookimpl(tryfirst=True, hookwrapper=True)
 def pytest_runtest_makereport(item):
     pytest_html = item.config.pluginmanager.getplugin("html")
